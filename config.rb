acbd4c1b1d1d24ea50932d5936730904dfcc5232
Verena Ortlieb <verena.ortlieb@gmail.com>
2016-03-25 14:03:04 +0100

    Remove HAML config

--- config.rb
+++ config.rb
@@ -16,9 +16,6 @@ page '/*.txt', layout: false
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
-# General configuration
-set :haml, { :ugly => true, :format => :html5 }
-
 # Enable blogging
 activate :blog do |blog|
     blog.sources = "posts/{year}-{month}-{day}-{title}.html"
