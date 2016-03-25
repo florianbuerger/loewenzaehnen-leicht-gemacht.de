08c110d9301cc3fbbfca68d518544367a3151b3b
Verena Ortlieb <verena.ortlieb@gmail.com>
2016-03-25 14:02:39 +0100

    Minify CSS & JS

--- config.rb
+++ config.rb
@@ -46,10 +46,8 @@ end
 
 # Build-specific configuration
 configure :build do
-  # Minify CSS on build
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_css
+  activate :minify_javascript
+  activate :gzip
   activate :minify_html
 end
